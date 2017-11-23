11fa81f6e11110363c05bcf37e3d0b2dbcffad3e
alecorguill <alecorguill@laposte.net>
2017-11-23 11:29:32 +0100

    config file

---
+++ header/util.hpp
@@ -0,0 +1,18 @@
+#ifndef UTIL_H
+#define UTIL_h
+
+typedef struct {
+  double Lx;
+  double Ly;
+  int Nx;
+  int Ny;
+  double D;
+  int choix;
+  int np;
+  double dx;
+  double dy;
+  double dt;
+} config_t;
+
+void parse_file(char* filename, config_t& c);
+#endif
---
+++ src/util.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+
+#include "util.hpp"
+
+using namespace std;
+
+void parse_file(char* filename, config_t&c){
+  ifstream file(filename);
+  
+  string line;
+  while(getline(file, line)){
+    stringstream iss(line);
+    string key;
+    if( getline(iss, key, '=') )
+      {
+	string value;
+	getline(iss, value);
+	const char * c_value = value.c_str();
+	if(key == "Lx")
+	  c.Lx = atof(c_value);
+	else if(key == "Ly")
+	  c.Ly = atof(c_value);
+	else if(key == "D")
+	  c.D = atof(c_value);
+	else if(key == "Nx"){
+	  c.Nx = atoi(c_value);
+	}
+	else if(key == "Ny")
+	  c.Ny = atoi(c_value);
+	else if(key == "choix")
+	  c.choix = atoi(c_value);
+      }
+  }
+}
